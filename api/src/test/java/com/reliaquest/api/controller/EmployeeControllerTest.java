617ae3d0445965e3405b80339f98ab59e0ac08bb
Ryan Carr <ryan.carr@ryca.dev>
2025-11-04 18:21:33 -0800

    Stubbed out tests for EmployeeController

---
+++ api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
@@ -0,0 +1,41 @@
+package com.reliaquest.api.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllEmployees_ShouldReturnAllEmployees_Success() {
+    }
+
+    @Test
+    void getEmployeesByNameSearch_ShouldReturnEmployeesByName_Success() {
+    }
+
+    @Test
+    void getEmployeeById_ShouldReturnEmployeeById_Success() {
+    }
+
+    @Test
+    void getHighestSalaryOfEmployees_ShouldReturnHighestSalaryOfEmployees_Success() {
+    }
+
+    @Test
+    void getTopTenHighestEarningEmployeeNames_ShouldReturnTopTenHighestEarningEmployeeNames_Success() {
+    }
+
+    @Test
+    void createEmployee_ShouldCreateEmployee_Success() {
+    }
+
+    @Test
+    void deleteEmployeeById_ShouldDeleteEmployeeById_Success() {
+    }
+}
